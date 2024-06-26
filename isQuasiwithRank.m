da4d9c29a3cc25106583872302c82f7193ee284e
Luis Miguel Varona <lbvarona@mta.ca>
2024-06-26 20:54:17 -0300

    I have fix'ed thine stuffins

--- isQuasiwithRank.m
+++ isQuasiwithRank.m
@@ -1,5 +1,7 @@
-function quasi = isQuasiOrthogonalizable(X)
+function [quasi, rank] = isQuasiwithRank(X)
     n = size(X, 2);
+    quasi = true;
+    rank = n*(n - 1)/2;
 
     % BEGIN: OrthoGraphComplement
     adjList = zeros(n, 2);
@@ -7,6 +9,7 @@ function quasi = isQuasiOrthogonalizable(X)
         %if any(not(ismembertol(X(:, j), 0)))
         for k = (j+1):n
             if not(isequaltol(X(:, j)'*X(:, k), 0))
+                rank = rank - 1;
                 for idx = [j, k]
                     if adjList(idx, 1) == 0
                         adjList(idx, 1) = 1;
@@ -14,7 +17,6 @@ function quasi = isQuasiOrthogonalizable(X)
                         adjList(idx, 2) = 1;
                     else
                         quasi = false;
-                        return
                     end
                 end
             end
@@ -23,36 +25,38 @@ function quasi = isQuasiOrthogonalizable(X)
     end
     % END: OrthoGraphComplement
 
-    % BEGIN: isPathSubgraph
-    % Determines whether the adjacency list A represents a subgraph of a path.
-    path = true;
-    row_sums = zeros(1, n);
-    for i = 1:n
-        for j = 1:2
-            if adjList(i, j) ~= 0
-                row_sums(i) = row_sums(i) + 1;
+    if quasi == true
+        % BEGIN: isPathSubgraph
+        % Determines whether the adjacency list A represents a subgraph of a path.
+        path = true;
+        row_sums = zeros(1, n);
+        for i = 1:n
+            for j = 1:2
+                if adjList(i, j) ~= 0
+                    row_sums(i) = row_sums(i) + 1;
+                end
+            end
+        end
+
+        % row_sums > 2 already checked in orthograph complement section above
+        if sum(row_sums <= 1) < 2
+            path = false;
+        else
+            visited = zeros(1, n);
+            for i = 1:n
+                if row_sums(i) == 1
+                    visited = travelPath(adjList, visited, i);
+                end
+            end
+            for i = 1:n
+                if visited(i) == 0 && row_sums(i) ~= 0
+                    path = false;
+                    break
+                end
             end
         end
     end
-
-    % row_sums > 2 already checked in orthograph complement section above
-    if sum(row_sums <= 1) < 2
-        path = false;
-    else
-        visited = zeros(1, n);
-        for i = 1:n
-            if row_sums(i) == 1
-                visited = travelPath(adjList, visited, i);
-            end
-        end
-        for i = 1:n
-            if visited(i) == 0 && row_sums(i) ~= 0
-                path = false;
-                break
-            end
-        end
-    end
-
+    
     % Iteratively visits and marks each node of A (if not
     % all nodes are marked, then A is not a path subgraph)
     function visited = travelPath(adjList, visited, j)
